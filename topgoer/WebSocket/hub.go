1ca3861883de483d4655f169b07b246e4c3c4ae3
tomoon <fengweiming@zork.com.cn>
2021-06-06 17:00:39 +0800

    add websocket example

--- topgoer/WebSocket/hub.go
+++ topgoer/WebSocket/hub.go
@@ -1,5 +1,7 @@
 package main
 
+import "encoding/json"
+
 type hub struct {
 	c map[*connection]bool
 	b chan []byte
@@ -17,10 +19,27 @@ var h = hub{
 func (h *hub) run()  {
 	for{
 		select {
-		case c := <- h.r
+		case c := <- h.r:
 			h.c[c] = true
 			c.data.Ip = c.ws.RemoteAddr().String()
 			c.data.Type = "handshake"
+			c.data.UserList = user_list
+			data_b, _ := json.Marshal(c.data)
+			c.sc <- data_b
+		case c := <- h.u:
+			if _, ok := h.c[c];ok{
+				delete(h.c,c)
+				close(c.sc)
+			}
+		case data := <-h.b :
+			for c:= range h.c {
+				select {
+				case c.sc <- data:
+				default:
+					delete(h.c,c)
+					close(c.sc)
+				}
+			}
 		}
 	}
 }
