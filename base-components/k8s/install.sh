46dccf32d8f015d655d23c1a990a4dde5a286edc
JavaLearning2020 <williamalex22@126.com>
2021-06-12 22:07:37 +0800

    add k8s components

---
+++ base-components/k8s/install.sh
@@ -0,0 +1,3 @@
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+##check
+##kubectl version --client
--- topgoer/WebSocket/server.go
+++ topgoer/WebSocket/server.go
@@ -11,7 +11,7 @@ func main() {
 	router := mux.NewRouter()
 	go h.run()
 	router.HandleFunc("/ws",myws)
-	if err := http.ListenAndServe("127.0.0.1:8080",router); err != nil{
+	if err := http.ListenAndServe("127.0.0.1:8181",router); err != nil{
 		fmt.Println("err:",err)
 	}
 }
