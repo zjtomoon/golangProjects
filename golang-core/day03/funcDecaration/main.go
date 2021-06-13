49030569c42446396f8fbb9f97227abdb6d46fba
JavaLearning2020 <williamalex22@126.com>
2021-06-13 20:50:26 +0800

    add func decaration

--- golang-core/day03/funcDecaration/main.go
+++ golang-core/day03/funcDecaration/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "fmt"
 
 //函数声明语句，用于Go代码调用汇编代码
 //func add(int,int) int
@@ -14,9 +15,13 @@ type ADD func(int,int) int
 
 var g ADD = add
 func main() {
-	f := func(int,int) int {
+	f := func(a,b int) int {
 		return a + b
 	}
 	g(1,2)
 	f(1,2)
+
+	//f和add的函数签名相同
+	fmt.Printf("%T\n",f) 	//func(int, int) int
+	fmt.Printf("%T\n",add)  //func(int, int) int
 }
