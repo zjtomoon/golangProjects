c508aedca8769b311473285f785d407fa6fbbd79
zjtomoon <zjtomoon@outlook.com>
2021-06-17 16:26:18 +0800

    Default Changelist

--- golang-core/day05/contextLib02/main.go
+++ golang-core/day05/contextLib02/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 type otherContext struct {
 	context.Context
@@ -9,14 +13,55 @@ type otherContext struct {
 func main() {
 	//使用context.Backgrond()构建一个WithCancel类型的上下文
 	ctxa,cancel := context.WithCancel(context.Background())
+
 	//work模拟运行并检测前端的退出通知
-	
+	go work(ctxa,"work1")
+
+	//使用WithDeadline包装前面的上下文对象ctxa
+	tm := time.Now().Add(3 * time.Second)
+	ctxb,_ := context.WithDeadline(ctxa,tm)
+
+	go work(ctxb,"work2")
+
+	//使用WithValue包装前面的上下文对象ctxb
+	oc := otherContext{ctxb}
+	ctxc := context.WithValue(oc,"key","andes,pass from main")
+
+	go workWithValue(ctxc,"work3")
+	//故意"sleep" 10秒，让work2、work3超时退出
+	time.Sleep(10 * time.Second)
+
+	//显示调用work1的cancel方法通知其退出
+	cancel()
+	//等待work1打印退出信息
+	time.Sleep(5 * time.Second)
+	fmt.Println("main stop")
 }
 
-func work()  {
-	
+func work(ctx context.Context,name string)  {
+	for  {
+		select {
+		case <- ctx.Done():
+			fmt.Printf("%s get msg to cancel \n",name)
+			return
+		default:
+			fmt.Printf("%s is running \n",name)
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
 
-func workWithValue()  {
-	
+//等待前端的退出通知，并试图获取Context传递的数据
+func workWithValue(ctx context.Context,name string)  {
+	for  {
+		select {
+		case <- ctx.Done():
+			fmt.Printf("%s get msg to cancel \n",name)
+			return
+		default:
+			value := ctx.Value("key").(string)
+			fmt.Printf("%s is running value = %s \n",name,value)
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
