02d34f19f0d38631fb6d60eebe582b08d9560941
zjtomoon <zjtomoon@outlook.com>
2021-06-25 20:07:25 +0800

    Default Changelist

---
+++ goinaction/chapter02/search/feed.go
@@ -0,0 +1,33 @@
+package search
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const dataFile  = "data/data.json"
+//feed包含我们需要处理的数据源的信息
+type Feed struct {
+	Name string `json:"site"`
+	URL  string `json:"link"`
+	Type string `json:"type"`
+}
+
+//RetrieveFeeds读取反序列化源数据文件
+func RetrieveFeeds() ([]*Feed,error) {
+	//打开文件
+	file,err := os.Open(dataFile)
+	if err != nil {
+		return nil,err
+	}
+	//当函数返回时，关闭文件
+	defer file.Close()
+
+	//将文件解码到一个切片里
+	//这个切片的每一项是一个指向一个Feed类型的指针
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	//这个函数不需要检查错误，调用者会做这件事
+	return feeds,err
+}
--- goinaction/chapter02/search/search.go
+++ goinaction/chapter02/search/search.go
@@ -4,5 +4,6 @@ var matchers = make(map[string]Matcher)
 
 //Run执行搜索逻辑
 func Run(searchTerm string)  {
+	//获取需要搜索的数据列表
 
 }
