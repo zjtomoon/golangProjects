34f9bc11e50c7adc585a76b07ad1520ce976e151
zjtomoon <zjtomoon@outlook.com>
2021-06-29 21:05:06 +0800

    default commit

--- examples/21weeks/iniTestofReflect/main.go
+++ examples/21weeks/iniTestofReflect/main.go
@@ -25,6 +25,11 @@ type RedisConfig struct {
 	Password	string	`ini:"password"`
 	Database	int		`ini:"database"`
 }
+// Config ...
+type Config struct {
+	MysqlConfig	`ini:"mysql"`
+	RedisConfig	`ini:"redis"`
+}
 
 func loadIni(fileName string,data interface{}) (err error) {
 	 // 0. 参数地校验
@@ -50,19 +55,58 @@ func loadIni(fileName string,data interface{}) (err error) {
 	 lineSlice := strings.Split(string(b),"\n")
 	 fmt.Printf("%#v\n",lineSlice)
 	 // 2. 一行一行地读数据
-	 // 2.1 如果是注释就跳过
-	 // 2.2 如果是[开头地就表示是节(section)
-	 // 2.3 如果不是[开头就是分割地键值对
+	 var structName string
+	 for idx,line := range lineSlice {
+		 // 去掉字符串首尾地空格
+		 line = strings.TrimSpace(line)
+		 // 2.1 如果是注释就跳过
+		 if strings.HasPrefix(line,";") || strings.HasPrefix(line,"#") {
+			 continue
+		 }
+	 	// 2.2 如果是[开头地就表示是节(section)
+		 if strings.HasPrefix(line,"[") {
+			if line[0] != '[' || line[len(line)-1] != ']' {
+				err = fmt.Errorf("line:%d syntax error",idx + 1)
+				return
+			}
+			//把这一行首尾地[]去掉，取到中间的内容把首尾的空格去掉拿到内容
+			sectionName := strings.TrimSpace(line[1:len(line)-1])
+			if len(sectionName) == 0 {
+				err = fmt.Errorf("line:%d syntax error",idx + 1)
+				return
+			}
+			//根据字符串sectionName去data里面根据反射找到对应的结构体
+			//v := reflect.ValueOf(data)
+			for i := 0 ; i < t.Elem().NumField() ; i++ {
+				field := t.Field(i)
+				if sectionName == field.Tag.Get("ini") {
+					//说明找到了对应的嵌套结构体，把字段名记下来
+					structName = field.Name
+					fmt.Printf("找到%s对应的嵌套结构体%s\n",sectionName,structName)
+					break
+				}
+			}
+		 } else {
+			// 2.3 如果不是[开头就是分割地键值对
+			// 1、以等号分割这一行，等号左边是key,等号右边是value
+			
+			// 2、根据structName 去 data里面把对应的嵌套结构体取出来
+			// 3、遍历嵌套结构体的每一个字段，判断tag是不是key
+			// 4、如果key == tag,给这个字段赋值
 
+		 }
+
+	 	
+	 }
 	 return
 }
 
 func main()  {
-	var mc MysqlConfig
-	err := loadIni("./config.ini",&mc)
+	var cfg Config
+	err := loadIni("./config.ini",&cfg)
 	if err != nil {
 		fmt.Printf("load ini failed,err:%v\n",err)
 		return
 	}
-	fmt.Println(mc.Address,mc.Port,mc.Username,mc.Password)
+	fmt.Println(cfg)
 }
