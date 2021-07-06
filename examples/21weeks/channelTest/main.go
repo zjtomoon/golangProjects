631aceed6ae65b2b4e4ffe6380f57bf786c46ecb
zjtomoon <zjtomoon@outlook.com>
2021-07-06 18:05:26 +0800

    default commit

---
+++ examples/21weeks/channelTest/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+//channel练习
+//1、启动一个goroutine,生成100个数发送到ch1
+//2、启动一个goroutine,从ch1中取值，计算其平方放到ch2中
+//3、在main中从ch22取值打印出来
+var wg sync.WaitGroup
+func f1(ch1 chan int) {
+	defer wg.Done()
+	for i := 0 ; i < 100 ;i++ {
+		ch1 <- i
+	}
+}
+
+func f2(ch1,ch2 chan int) {
+	for x:= range ch1 {
+		ch2 <- x * x
+	}
+}
+
+func main() {
+	a := make(chan int,50)
+	b := make(chan int,100)
+	wg.Add(2)
+	go f1(a)
+	go f2(a,b)
+	for ret := range b {
+		fmt.Println(ret)
+	}
+}
