03048740dcb8e758c564e3c4d5aea789243b6bb0
zjtomoon <zjtomoon@outlook.com>
2021-07-06 22:34:56 +0800

    default commit

--- examples/21weeks/channelTest/main.go
+++ examples/21weeks/channelTest/main.go
@@ -10,24 +10,34 @@ import (
 //2、启动一个goroutine,从ch1中取值，计算其平方放到ch2中
 //3、在main中从ch22取值打印出来
 var wg sync.WaitGroup
+var once sync.Once
 func f1(ch1 chan int) {
 	defer wg.Done()
 	for i := 0 ; i < 100 ;i++ {
 		ch1 <- i
 	}
+	close(ch1)
 }
 
 func f2(ch1,ch2 chan int) {
-	for x:= range ch1 {
-		ch2 <- x * x
+	defer wg.Done()
+	for {
+		x,ok := <-ch1
+		if !ok {
+			break
+		}
+		ch2 <- x*x
 	}
+	//close(ch2)
+	once.Do(func() { close(ch2) }) //确保某个操作只执行一次
 }
 
 func main() {
 	a := make(chan int,50)
 	b := make(chan int,100)
-	wg.Add(2)
+	wg.Add(3)
 	go f1(a)
+	go f2(a,b)
 	go f2(a,b)
 	for ret := range b {
 		fmt.Println(ret)
