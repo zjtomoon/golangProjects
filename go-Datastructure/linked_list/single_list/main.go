74cb2b54c09018d6b7170e97798cd834594c3215
zjtomoon <zjtomoon@outlook.com>
2021-08-11 21:17:57 +0800

    单链表模拟实现

--- go-Datastructure/linked_list/single_list/main.go
+++ go-Datastructure/linked_list/single_list/main.go
@@ -60,6 +60,91 @@ func InsertHeroNode2(head *HeroNode,newHeroNode *HeroNode)  {
 	}
 }
 
+//删除一个节点
+func DelHeroNode(head *HeroNode,id int)  {
+	temp := head
+	flag := false
+	//删除要删除的节点的no,和temp的下一个节点的no比较
+	for {
+		if temp.next == nil { //说明到了链表的最后
+			break
+		}else if temp.next.no == id {
+			//说明找到了
+			flag = true
+			break
+		}
+		temp = temp.next
+	}
+	if flag { //找到，删除
+		temp.next = temp.next.next
+	} else {
+		fmt.Println("抱歉，要删除的id不存在")
+	}
+}
+
+
+//显示链表的所有节点信息
+func ListHeroNode(head *HeroNode)  {
+	//1. 创建一个辅助节点
+	temp := head
+	//先判断该链表是不是一个空的链表
+	if temp.next == nil {
+		fmt.Println("空空如也...")
+		return
+	}
+	//2. 遍历这个链表
+	for {
+		fmt.Printf("[%d ,%s , %s] ==> ",temp.next.no,temp.next.name,temp.next.nickname)
+		//判断是否链表后
+		temp = temp.next
+		if temp.next == nil {
+			break
+		}
+	}
+}
+
 func main() {
+	//1. 先创建一个头节点
+	head := &HeroNode{}
+
+	//2. 创建一个新的HeroNode
+	hero1 := &HeroNode{
+		no:1,
+		name:"宋江",
+		nickname:"及时雨",
+	}
+
+	hero2 := &HeroNode{
+		no:2,
+		name:"卢俊义",
+		nickname:"玉麒麟",
+	}
+	hero3 := &HeroNode{
+		no:3,
+		name:"林冲",
+		nickname:"豹子头",
+	}
+/*	hero4 := &HeroNode{
+		no:4,
+		name:"林冲",
+		nickname:"智多星",
+	}*/
+
+	//3. 加入
+	InsertHeroNode2(head,hero3)
+	InsertHeroNode2(head,hero1)
+	InsertHeroNode2(head,hero2)
+
+	//4. 显示
+	ListHeroNode(head)
+
+	//5. 删除
+	fmt.Println()
+	DelHeroNode(head,1)
+	DelHeroNode(head,3)
+
+	fmt.Println("===========================")
+
+	ListHeroNode(head)
 
 }
