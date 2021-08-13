e31739db66d5948d87b0b94969701f88818a81ed
zjtomoon <zjtomoon@outlook.com>
2021-08-13 18:54:04 +0800

    约瑟夫问题

--- go-Datastructure/Josephu/main.go
+++ go-Datastructure/Josephu/main.go
@@ -24,7 +24,7 @@ func AddBoy(num int) *Boy {
 	}
 
 	//循环的构建这个环形链表
-	for i := 0; i <= num; i++ {
+	for i := 1; i <= num; i++ {
 		boy := &Boy{
 			No: i,
 		}
@@ -60,9 +60,66 @@ func ShowBoy(first *Boy)  {
 		if curBoy.Next == first {
 			break
 		}
+		//curBot移动到下一个
+		curBoy = curBoy.Next
 	}
 }
 
-func main() {
+/*
+设编号为1，2，… n的n个人围坐一圈，约定编号为k（1<=k<=n）
+的人从1开始报数，数到m 的那个人出列，它的下一位又从1开始报数，
+数到m的那个人又出列，依次类推，直到所有人出列为止，由此产生一个出队编号的序列
+*/
 
+//分析思路
+//1. 编写一个函数，PlayGame(first *Boy, startNo int, countNum int) 
+//2. 最后我们使用一个算法，按照要求，在环形链表中留下最后一个人
+
+func PlayGame(first *Boy,startNo int,countNum int)  {
+	//1. 空的链表我们单独处理
+	if first.Next == nil {
+		fmt.Println("空的链表，没有小孩")
+		return
+	}
+	//留一个
+	//2. 需要定义辅助指针，帮助我们删除小孩
+	tail := first
+	//3. 让tail指向环形链表的最后一个小孩，这个非常重要
+	//因为tail在删除小孩时需要使用到
+	for {
+		if tail.Next == first { //说明tail到了最后的小孩
+			break
+		}
+		tail = tail.Next
+	}
+	//4. 让first移动到startNo [后面我们删除小孩，就以first为准]
+	for i := 1; i <= startNo - 1; i++ {
+		first = first.Next
+		tail = tail.Next
+	}
+	fmt.Println()
+	//5.开始数countNum.然后就删除first指向的小孩
+	for {
+		//开始数countNum-1次
+		for i := 1; i < countNum - 1; i++ {
+			first = first.Next
+			tail = tail.Next
+		}
+		fmt.Printf("小孩编号为%d 出圈 \n",first.No)
+		//删除first指向的小孩
+		first = first.Next
+		tail.Next = first
+		//判断如果tail == first ,圈子中只有一个小孩
+		if tail == first {
+			break
+		}
+	}
+	fmt.Printf("小孩编号为 %d 出圈 \n",first.No)
+}
+
+func main() {
+	first := AddBoy(500)
+	//显示
+	ShowBoy(first)
+	PlayGame(first,20,31)
 }
