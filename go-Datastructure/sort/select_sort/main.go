bd3239adf6a95f8b0d19dc53627891d62c408093
zjtomoon <zjtomoon@outlook.com>
2021-08-14 19:05:15 +0800

    选择排序

--- go-Datastructure/sort/select_sort/main.go
+++ go-Datastructure/sort/select_sort/main.go
@@ -37,6 +37,57 @@ func SelectSort(arr *[80000]int)  {
 		//fmt.Printf("第%d次 %v\n",j+1,*arr)
 	}
 
+	/*
+	max = arr[1]
+	maxIndex = 1
+	//2. 遍历后面 2---[len(arr) -1] 比较
+	for i := 1 + 1; i < len(arr); i++ {
+		if max < arr[i] { //找到真正的最大值
+			max = arr[i]
+			maxIndex = i
+		}
+	}
+	//交换
+	if maxIndex != 1 {
+		arr[1], arr[maxIndex] = arr[maxIndex], arr[1]
+	}
+
+	fmt.Println("第2次 ", *arr)
+
+	
+
+	max = arr[2]
+	maxIndex = 2
+	//2. 遍历后面 3---[len(arr) -1] 比较
+	for i := 2 + 1; i < len(arr); i++ {
+		if max < arr[i] { //找到真正的最大值
+			max = arr[i]
+			maxIndex = i
+		}
+	}
+	//交换
+	if maxIndex != 2 {
+		arr[2], arr[maxIndex] = arr[maxIndex], arr[2]
+	}
+
+	fmt.Println("第3次 ", *arr)
+
+	max = arr[3]
+	maxIndex = 3
+	//2. 遍历后面 4---[len(arr) -1] 比较
+	for i := 3 + 1; i < len(arr); i++ {
+		if max < arr[i] { //找到真正的最大值
+			max = arr[i]
+			maxIndex = i
+		}
+	}
+	//交换
+	if maxIndex != 3 {
+		arr[3], arr[maxIndex] = arr[maxIndex], arr[3]
+	}
+
+	fmt.Println("第4次 ", *arr)*/
+
 }
 
 func main() {
