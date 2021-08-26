43267fce25e3c40d0658543aa100ef52cc9d5b1d
zjtomoon <zjtomoon@outlook.com>
2021-08-26 17:39:58 +0800

    gin学习

--- Go_frameworks/gin/gin_study/routes_group/main.go
+++ Go_frameworks/gin/gin_study/routes_group/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 //routes group是为了管理一些相同的URL
 
 func main() {
+	//1.创建路由
+	//默认使用了2个中间件Logger(),Recovery()
+	r := gin.Default()
+	//路由组1 ，处理GET请求
 
 }
