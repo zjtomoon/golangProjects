43396dac54a222c4723ece56ab5d46f20f092f7d
zjtomoon <zjtomoon@outlook.com>
2021-08-29 22:25:56 +0800

    go语言标准库学习

---
+++ go_standard_Lib/IO_test/TeeReader/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"strings"
+	"os"
+)
+
+/*
+
+TeeReader 返回一个 Reader，它将从 r 中读到的数据写入 w 中。所有经由它处理的
+从 r 的读取都匹配于对应的对 w 的写入。它没有内部缓存，即写入必须在读取完成前
+完成。任何在写入时遇到的错误都将作为读取错误返回。
+也就是说，我们通过 Reader 读取内容后，会自动写入到 Writer 中去
+
+ */
+
+func main() {
+	reader := io.TeeReader(strings.NewReader("Go语言中文网"),os.Stdout)
+	reader.Read(make([]byte,20))
+}
