4e9419578b26796da3b127cea40c59407ffedcef
zjtomoon <zjtomoon@outlook.com>
2021-08-30 11:21:21 +0800

    go语言标准库

--- go_standard_Lib/budio_test/ReadSlice/main.go
+++ go_standard_Lib/budio_test/ReadSlice/main.go
@@ -7,11 +7,17 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(strings.NewReader("http://studygolang.com.\nIt is the home of gophers"))
-	line, _ := reader.ReadSlice('\n')
-	fmt.Printf("the line : %s \n", line)
-	//这里可以换上任意的bufio的Read/Write操作
-	n, _ := reader.ReadSlice('\n')
-	fmt.Printf("the line : %s\n", line)
-	fmt.Println(string(n))
+	/*	reader := bufio.NewReader(strings.NewReader("http://studygolang.com.\nIt is the home of gophers"))
+		line, _ := reader.ReadSlice('\n')
+		fmt.Printf("the line : %s \n", line)
+		//这里可以换上任意的bufio的Read/Write操作
+		n, _ := reader.ReadSlice('\n')
+		fmt.Printf("the line : %s\n", line)
+		fmt.Println(string(n))*/
+
+	reader := bufio.NewReaderSize(strings.NewReader("http://studygolang.com"), 16)
+	line, err := reader.ReadSlice('\n')
+	fmt.Printf("line: %s \t error: %s\n", line, err)
+	line, err = reader.ReadSlice('\n')
+	fmt.Printf("line: %s \t error : %s \n", line, err)
 }
