6e7940f64e13253768f463e4a0cb68930261a270
zjtomoon <zjtomoon@outlook.com>
2021-09-11 11:15:49 +0800

    gin框架博客项目实战

--- my-demos/gin-blog/service/category.go
+++ my-demos/gin-blog/service/category.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"golangProjects/my-demos/gin-blog/database/db"
+	"golangProjects/my-demos/gin-blog/model"
+)
+
+//获取所有分类
+func GetALLCategoryList() (categoryList []*model.Category, err error) {
+	categoryList, err = db.GetAllCategoryList()
+	if err != nil {
+		return
+	}
+	return
+}
