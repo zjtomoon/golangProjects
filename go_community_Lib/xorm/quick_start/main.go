1c6c637fe16b4c4a51468db9baf70ba07e619b4e
zjtomoon <zjtomoon@outlook.com>
2021-09-15 16:25:31 +0800

    xorm第三方库学习

---
+++ go_community_Lib/xorm/quick_start/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"time"
+	"xorm.io/xorm"
+)
+
+type User struct {
+	Id      int64
+	Name    string
+	Salt    string
+	Age     int
+	Passwd  string    `xorm:"varchar(200)"`
+	Created time.Time `xorm:"created"`
+	Updated time.Time `xorm:"updated"`
+}
+
+func main() {
+	engine,err := xorm.NewEngine("mysql","root:123@/test?charset=utf8")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = engine.Sync2(new(User))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
