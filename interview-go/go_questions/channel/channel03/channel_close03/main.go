7f9a3fc7bf2c96a4c7a9583ad5e215ef4001f1fb
zjtomoon <zjtomoon@outlook.com>
2021-09-18 22:08:17 +0800

    default commit

--- interview-go/go_questions/channel/channel03/channel_close03/main.go
+++ interview-go/go_questions/channel/channel03/channel_close03/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"math/rand"
 	"time"
+	"strconv"
+	"fmt"
 )
 
 func main() {
@@ -15,5 +17,61 @@ func main() {
 	dataCh := make(chan int,100)
 	stopCh := make(chan struct{})
 
+	//必须是1个缓存通道
+	toStop := make(chan string,1)
 
+	var stoppedBy string
+
+	//moderator
+	go func() {
+		stoppedBy = <- toStop
+		close(stopCh)
+	}()
+
+	//senders
+	for i := 0 ; i < NumReceivers ; i++ {
+		go func(id string) {
+			for {
+				value := rand.Intn(Max)
+				if value == 0 {
+					select {
+					case toStop <- "sender#" + id:
+					default:
+					}
+					return
+				}
+				select {
+				case <- stopCh:
+					return
+				case dataCh <- value:
+				}
+			}
+		}(strconv.Itoa(i))
+	}
+
+	//receivers
+	for i := 0 ; i < NumReceivers ; i++ {
+		go func(id string) {
+			for {
+				select {
+				case <- stopCh:
+					return
+				case value := <- dataCh:
+					if value == Max -1 {
+						select {
+						case toStop <- "receive#" + id:
+						default:
+						}
+						return
+					}
+					fmt.Println(value)
+				}
+			}
+		}(strconv.Itoa(i))
+	}
+
+	select {
+	case <- time.After(time.Hour):
+		
+	}
 }
--- interview-go/tmp.md
+++
@@ -1,4 +0,0 @@
-[go_questions](https://www.topgoer.cn/docs/goquestions/goquestions-1cjh24lli0ear)
-
-[go语言面试题](https://www.topgoer.cn/docs/gomianshiti/mianshiti)
-
