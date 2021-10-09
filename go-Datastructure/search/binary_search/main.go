5a64651a0f0a1ba8fd717ec0d4caf82e0a202d1a
zjtomoon <zjtomoon@outlook.com>
2021-10-09 19:50:56 +0800

    二分法查找无序数组

--- go-Datastructure/search/binary_search/main.go
+++ go-Datastructure/search/binary_search/main.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1,2,3,4,5,6,7,8,9,10}
+	//arr := []int{1,2,3,4,5,6,7,8,9,10}
+	arr := []int{1, 2, 7, 9, 3, 10, 3, 8, 5, 4, 6}
+	QuickSort(0, len(arr)-1, arr)
+	fmt.Println(arr)
 	k := 9
-	n := BinarySearch(arr,k)
-	fmt.Printf("要查找的数字[%d]在位置[%d]",k,n)
+	n := BinarySearch(arr, k)
+	fmt.Printf("要查找的数字[%d]在位置[%d]", k, n)
 }
 
-func BinarySearch(s []int,k int) int {
-	low,high := 0,len(s)-1
+func BinarySearch(s []int, k int) int {
+	low, high := 0, len(s)-1
 	for low < high {
 		mid := (low + high) / 2
 		if s[mid] < k {
@@ -24,12 +27,44 @@ func BinarySearch(s []int,k int) int {
 	return -1
 }
 
-func QuickSort(left int,right int,arr *[...]int)  {
-	l,r := left,right
-	privot := arr[(left + right)/2]
+func QuickSort(left int, right int, arr []int) {
+	l, r := left, right
+	privot := arr[(left+right)/2]
 	temp := 0
 
 	for l < r {
+		for arr[l] < privot {
+			l++
+		}
+		for arr[r] > privot {
+			r--
+		}
+		if l >= r {
+			break
+		}
 
+		//交换
+		temp = arr[l]
+		arr[l] = arr[r]
+		arr[r] = temp
+
+		if arr[l] == privot {
+			r--
+		}
+		if arr[r] == privot {
+			l++
+		}
+	}
+
+	if l == r {
+		l++
+		r--
+	}
+
+	if left < r {
+		QuickSort(left, r, arr)
+	}
+	if right > l {
+		QuickSort(l, right, arr)
 	}
 }
