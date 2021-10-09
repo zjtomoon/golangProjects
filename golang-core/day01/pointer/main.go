fc117cd576db59fc51630d30cac0a90cf004ff34
zjtomoon <zjtomoon@outlook.com>
2021-10-09 19:54:56 +0800

    default commit

--- golang-core/day01/pointer/main.go
+++ golang-core/day01/pointer/main.go
@@ -3,28 +3,29 @@ package main
 import (
 	"fmt"
 )
-func main(){
-	var a = 11;
+
+func main() {
+	var a = 11
 	p := &a
-	fmt.Println(a,*p)//a和*p都为11
+	fmt.Println(a, *p) //a和*p都为11
 
 	type User struct {
 		name string
-		age int
+		age  int
 	}
 	andes := User{
 		name: "andes",
-		age: 18,
+		age:  18,
 	}
 	q := &andes
 	fmt.Println(q.name)
 	/**
 	go不支持指针的运算
 	go 由于支持垃圾回收，如果支持指针运算，则会给垃圾回收的实现带来不便 禁止p++
-	 */
-	fmt.Println(sum(3,5))
+	*/
+	fmt.Println(sum(3, 5))
 }
-func sum (a ,b int) *int {
-	sum := a+b
+func sum(a, b int) *int {
+	sum := a + b
 	return &sum
 }
