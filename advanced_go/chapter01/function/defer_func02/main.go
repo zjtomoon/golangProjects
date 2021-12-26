8ed6f5931aacdc7e155eed9e498ea994e1afb50f
zjtomoon <zjtomoon@outlook.com>
2021-12-26 12:20:05 +0800

    go语言高级编程学习

--- advanced_go/chapter01/function/defer_func02/main.go
+++ advanced_go/chapter01/function/defer_func02/main.go
@@ -1,8 +1,8 @@
 package main
 
 //func main() {
-//	for i:= 0;i<3 ;i++  {
-//		i := i
+//	for i := 0; i < 3; i++ {
+//		i := i //定义一个循环体内局部变量i
 //		defer func() {
 //			println(i) // 2 1 0
 //		}()
@@ -12,6 +12,8 @@ package main
 func main() {
 	for i := 0; i < 3; i++ {
 		defer func(i int) {
+			//通过函数传入i
+			//defer 语句会马上对调用参数求值
 			println(i) // 2 1 0
 		}(i)
 	}
