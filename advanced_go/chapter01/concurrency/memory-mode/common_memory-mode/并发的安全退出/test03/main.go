f9e5def49dbe782c73f25fbc523f27e3f6c35a76
zjtomoon <zjtomoon@outlook.com>
2021-12-27 16:39:30 +0800

    go语言高级编程学习

--- advanced_go/chapter01/concurrency/memory-mode/common_memory-mode/并发的安全退出/test03/main.go
+++ advanced_go/chapter01/concurrency/memory-mode/common_memory-mode/并发的安全退出/test03/main.go
@@ -5,13 +5,13 @@ import (
 	"time"
 )
 
-func worker(channel chan bool)  {
+func worker(channel chan bool) {
 	for {
 		select {
 		default:
 			fmt.Println("hello")
 			//正常工作
-		case <- channel:
+		case <-channel:
 			//退出
 		}
 	}
@@ -20,9 +20,11 @@ func worker(channel chan bool)  {
 func main() {
 	channel := make(chan bool)
 
-	for i := 0 ; i < 10 ; i++ {
+	for i := 0; i < 10; i++ {
 		go worker(channel)
 	}
-	 time.Sleep(time.Second)
+	time.Sleep(time.Second)
 	close(channel)
+	//管道的发送操作和接收操作是一一对应的，如果要停止多个goroutine可能需要创建相同数量的管道，代价太大。
+	//可以通过close关闭一个管道来实现广播的效果，所有从关闭管道接收的操作均会收到一个零值和一个可选的失败标记。
 }
--- advanced_go/chapter01/concurrency/memory-mode/common_memory-mode/并发的安全退出/test04/main.go
+++ advanced_go/chapter01/concurrency/memory-mode/common_memory-mode/并发的安全退出/test04/main.go
@@ -6,29 +6,36 @@ import (
 	"time"
 )
 
-func worker(wg *sync.WaitGroup,channel chan bool)  {
+func worker(wg *sync.WaitGroup, cancel chan bool) {
 	defer wg.Done()
 
 	for {
 		select {
 		default:
 			fmt.Println("hello")
-		case <- channel:
+		case <-cancel:
 			return
 		}
 	}
 }
 
 func main() {
-	channel := make(chan bool)
+	cancel := make(chan bool)
 
 	var wg sync.WaitGroup
-	for i := 0 ; i < 10 ; i++ {
+	defer wg.Wait()
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go worker(&wg,channel)
+		go worker(&wg, cancel)
 	}
 
 	time.Sleep(time.Second)
-	close(channel)
-	wg.Wait()
+	close(cancel)
+	//wg.Wait()
 }
+
+/*
+	我们通过close来关闭cancel管道向多个goroutine广播退出的指令。不过这个程序依然不够稳健：
+	当每个Goroutine收到退出指令退出时会进行一定的清理工作，但是退出的清理工作并不能保证被完成，
+	因为main线程并没有等待各个工作goroutine退出工作完成的机制。故可以结合sync.WaitGroup来改进
+*/
