0a31380e015cc10be023f8f84789a0189eec4a71
zjtomoon <zjtomoon@outlook.com>
2021-12-29 20:54:47 +0800

    go语言实现反向代理

---
+++ go_standard_Lib/other/net_http/reverse_proxy/go.mod
@@ -0,0 +1,3 @@
+module golangProjects/go_standard_Lib/other/net_http/reverse_proxy
+
+go 1.15
---
+++ go_standard_Lib/other/net_http/reverse_proxy/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		return nil, err
+	}
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	originalDirector := proxy.Director
+
+	proxy.Director = func(req *http.Request) {
+		originalDirector(req)
+		modifyRequest(req)
+	}
+
+	proxy.ModifyResponse = modifyResponse()
+	proxy.ErrorHandler = errorHandler()
+	return proxy, nil
+}
+
+func modifyRequest(req *http.Request) {
+	req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
+}
+
+func errorHandler() func(http.ResponseWriter, *http.Request, error) {
+	return func(w http.ResponseWriter, req *http.Request, err error) {
+		fmt.Printf("Got error while modifying response: %v \n", err)
+		return
+	}
+}
+
+func modifyResponse() func(response *http.Response) error {
+	return func(resp *http.Response) error {
+		return errors.New("response body is invalid")
+	}
+}
+
+func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	proxy, err := NewProxy("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", ProxyRequestHandler(proxy))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
