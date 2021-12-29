e029973d662674a9581cdcfbe77dab1f6d008d16
zjtomoon <zjtomoon@outlook.com>
2021-12-29 16:20:26 +0800

    go语言高级编程

--- advanced_go/chapter01/concurrency/memory-mode/common_memory-mode/context包/test02/main.go
+++ advanced_go/chapter01/concurrency/memory-mode/common_memory-mode/context包/test02/main.go
@@ -45,8 +45,8 @@ func main() {
 	ch := GenerateNatural(ctx) //自然数序列：2，3，4，...
 	for i := 0 ; i < 1000 ; i++ {
 		prime := <- ch //新出现的素数
-		fmt.Printf("%v : %v\n",i+1,prime)
 		ch = PrimeFilter(ctx,ch,prime) //基于新素数构造的过滤器
+		fmt.Printf("%v : %v\n",i+1,prime)
 	}
 	cancel()
 }
