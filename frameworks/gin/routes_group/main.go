042c5bfdf360a9174eda85e5180d0293fed22e22
zjtomoon <zjtomoon@outlook.com>
2022-01-06 15:09:58 +0800

    gin复习

--- frameworks/gin/routes_group/main.go
+++ frameworks/gin/routes_group/main.go
@@ -16,23 +16,24 @@ func main() {
 	v1 := r.Group("/v1")
 	// {} 是书写规范
 	{
-		v1.GET("/login",login)
-		v1.GET("submut",submit)
+		v1.GET("/login", login)
+		v1.GET("submut", submit)
 	}
+	//路由组2 ，处理POST请求
 	v2 := r.Group("/v2")
 	{
-		v2.POST("/login",login)
-		v2.POST("/submit",submit)
+		v2.POST("/login", login)
+		v2.POST("/submit", submit)
 	}
 	r.Run(":8000")
 }
 
-func login(c *gin.Context)  {
-	name := c.DefaultQuery("name","jack")
-	c.String(200,fmt.Sprintf("hello %s\n",name))
+func login(c *gin.Context) {
+	name := c.DefaultQuery("name", "jack")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
 }
 
-func submit(c *gin.Context)  {
-	name := c.DefaultQuery("name","lily")
-	c.String(200,fmt.Sprintf("hello %s\n",name))
+func submit(c *gin.Context) {
+	name := c.DefaultQuery("name", "lily")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
 }
