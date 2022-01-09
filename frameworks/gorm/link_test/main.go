a13a859270f59c9c825c8d562a476dd119e980e8
zjtomoon <zjtomoon@outlook.com>
2022-01-09 11:06:33 +0800

    gorm框架学习

--- frameworks/gorm/link_test/main.go
+++ frameworks/gorm/link_test/main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main() {
-	db, err := gorm.Open("mysql", "tom:123@(127.0.0.1:3306)/blog_data?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "tom:123@(127.0.0.1:3306)/gorm_test?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println(err)
 		panic("--数据库连接失败")
 	}
+	fmt.Println("--连接数据库成功")
 	defer db.Close()
 }
