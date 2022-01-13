da716e6d2ca5a4e8c3219f6e5357fb83fb075888
zjtomoon <zjtomoon@outlook.com>
2022-01-13 21:00:43 +0800

    grpc学习

---
+++ frameworks/grpc/protobuf/grpc_auth/go.mod
@@ -0,0 +1,7 @@
+module golangProjects/frameworks/grpc/protobuf/grpc_auth
+
+go 1.15
+require (
+	google.golang.org/grpc v1.43.0
+	google.golang.org/protobuf v1.27.1
+)
---
+++ frameworks/grpc/protobuf/grpc_auth/server/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "golangProjects/frameworks/grpc/protobuf/grpc_auth/proto/hello"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
+	"net"
+)
+
+const (
+	Address = "127.0.0.1:50052"
+)
+
+type HelloService struct {
+}
+
+var helloService = HelloService{}
+
+func (h HelloService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	resp := new(pb.HelloResponse)
+	resp.Message = fmt.Sprintf("Hello %s.", in.Name)
+
+	return resp, nil
+}
+
+// auth 验证token
+func auth(ctx context.Context) error {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return grpc.Errorf(codes.Unauthenticated, "无Token认证信息")
+	}
+
+	var (
+		appid  string
+		appkey string
+	)
+	if val, ok := md["appid"]; ok {
+		appid = val[0]
+	}
+
+	if val, ok := md["appkey"]; ok {
+		appkey = val[0]
+	}
+
+	if appid != "101010" || appkey != "i am key" {
+		return grpc.Errorf(codes.Unauthenticated, "Token认证信息无效: appid = %s,appkey = %s", appid, appkey)
+	}
+
+	return nil
+}
+
+// interceptor
+func interceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	err := auth(ctx)
+	if err != nil {
+		return nil, err
+	}
+	//继续处理请求
+	return handler(ctx, req)
+}
+
+func main() {
+	listen, err := net.Listen("tcp", Address)
+	if err != nil {
+		grpclog.Fatalf("Failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+
+	//TLS认证
+	creds, err := credentials.NewServerTLSFromFile("../../")
+}
