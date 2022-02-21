5b13f4db34bdbe585925f32f8febf1bd005d5cec
zjtomoon <zjtomoon@outlook.com>
2022-02-21 09:36:04 +0800

    slice超过cap扩容后内存地址变化

--- golang-core/day01/slice/slice02/main.go
+++ golang-core/day01/slice/slice02/main.go
@@ -7,29 +7,34 @@ func main() {
 	b := make([]int, 2, 4)
 	c := a[0:3]
 
-	fmt.Println("len(b) = ",len(b))
-	fmt.Println("cap(b) = ",cap(b))
+	fmt.Println("len(b) = ", len(b))
+	fmt.Println("cap(b) = ", cap(b))
+	fmt.Println("b = ", b)
+	fmt.Printf("b address = %p \n", b)
 
 	b = append(b, 1)
-	fmt.Println("c = ",c)
-	fmt.Println("b = ",b)
-	fmt.Println("len(b) = ",len(b))
-	fmt.Println("cap(b) = ",cap(b))
+	fmt.Println("c = ", c)
+	fmt.Println("b = ", b)
+	fmt.Printf("b address = %p \n", b)
+
+	fmt.Println("len(b) = ", len(b))
+	fmt.Println("cap(b) = ", cap(b))
 
 	b = append(b, c...)
-	fmt.Println("b = ",b)
-	fmt.Println("len(b) = ",len(b))
-	fmt.Println("cap(b) = ",cap(b))
+	fmt.Println("b = ", b)
+	fmt.Println("len(b) = ", len(b))
+	fmt.Println("cap(b) = ", cap(b))
+	fmt.Printf("b address = %p \n", b) // slice扩容超过cap后，地址会变
 
 	d := make([]int, 2, 3)
 	copy(d, c) //copy只会复制d和C中长度最小的
-	fmt.Println("d = ",d)
-	fmt.Println("len(d) = ",len(d))
-	fmt.Println("cap(d) = ",cap(d))
+	fmt.Println("d = ", d)
+	fmt.Println("len(d) = ", len(d))
+	fmt.Println("cap(d) = ", cap(d))
 
 	str := "hello 世界！"
 	e := []byte(str) //将字符串转化为[]byte类型切片
 	f := []rune(str) //将字符类型转换为[]rune类型切片
-	fmt.Println("e = ",e)
-	fmt.Println("f = ",f)
+	fmt.Println("e = ", e)
+	fmt.Println("f = ", f)
 }
