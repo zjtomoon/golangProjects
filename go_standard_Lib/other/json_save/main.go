6d8b3cb9e1863670e5798eaa60634387be08b07f
zjtomoon <zjtomoon@outlook.com>
2022-04-18 14:31:32 +0800

    json save

---
+++ go_standard_Lib/other/json_save/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type ColorGroup struct {
+	ID     int
+	Name   string
+	Colors []string
+}
+
+func main() {
+	group := ColorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+	b, err := json.Marshal(group)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	//生成json文件
+	err = ioutil.WriteFile("test.json", b, os.ModeAppend)
+	if err != nil {
+		return
+	}
+	var data interface{}
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println("data", data) //map[ID:1 Name:Reds Colors:[Crimson Red Ruby Maroon]]
+	dataJson := data.(map[string]interface{})["Colors"]
+	fmt.Println("dataJson", dataJson) //[Crimson Red Ruby Maroon]
+	b11, err := json.Marshal(dataJson)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	err = ioutil.WriteFile("test11.json", b11, os.ModeAppend)
+	if err != nil {
+		return
+	}
+}
